1e0593705bd14ef4da71cd030585aa4ff8526c21
Xzib <syedzohaibali94@gmail.com>
2019-04-26 18:20:49 +0500

    form developed

--- test.php
+++ test.php
@@ -1,11 +1,52 @@
 <?php
+  $error = "";
+  if($_POST){
+
+    if(!$_POST["email"]){
+      $error .= "An email adress is required.<br>";
+    }
+
     
+    if(!$_POST["subject"]){
+      $error .= "A subject is required.<br>";
+    }
+    
+    if(!$_POST["content"]){
+      $error .= "content  is required.<br>";
+    }
+    if($_POST["email"]&&filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)===false){
+      $error .= "the email address is invalid.<br>";
+      
+    }
+
+    if($error != ""){
+      $error = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form</strong></p>'. $error .'</div>';
+  }
+
+  
+  else{
+    $emailTo = 'syedzohaibali94@gmail.com';
+    $subject = $_POST['subject'];
+    $content = $_POST['content'];
+    $headers = 'From: '.$_POST['email'];
+
+    if(mail($emailTo,$subject,$content,$headers)){
+      $successMessage = '<div class="alert alert-success" role="alert"><strong>Message sent successfully</strong></div>'; 
+    }
+
+  }
 
 
+} 
 ?>
 
 
-<html lang="en">
+
+
+
+
+
+<html   lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -17,41 +58,43 @@
     <title>test</title>
   </head>
   <body>
-    <form>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-</form>
+      <p><?echo $error.$successMessage; ?>
+      <div class="container">
+            <form method="post">
+                    <div id="error"></div>
+                    <div class="form-group">
+                      <label for="email">Email address</label>
+                      <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                    </div>
+                    <div class="form-group">
+                      <label for="Subject">Subject</label>
+                      <input type="text" class="form-control" name="subject" id="Subject">
+                        
+                    </div>
+                   
+                    <div class="form-group">
+                      <label for="content">What would you like to ask us</label>
+                      <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                    </div>
 
+                    <div class="form-group">
+                            <button class="btn btn-success" id="exampleFormControlTextarea1" >Submit</textarea>
+                          </div>
+                          
+            </form>
+        </div>
+
+        
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    
+    
+    
+
+
+</body>
 </html>
